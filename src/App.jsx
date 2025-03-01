65209beb3c57919efc55af5ebbf65aa3c806b372
Vicky raj <78330753+mas173@users.noreply.github.com>
2025-03-01 20:17:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
-import Sidebar from './components/sidebar/sidebar'
+import Sidebar from './components/sidebar/Sidebar'
 import Heading from './components/sidebar/main/Heading'
 import "./App.css"
 import Content from './components/sidebar/main/Content'
